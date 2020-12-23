6f4d7829340637537789666bff8a2b538bfba13c
bx <edlislbx@yeah.net>
2020-12-24 03:06:35 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import List from './List';
+
+function App() {
+  return (
+    <div className='main'>
+      <List />
+    </div>
+  );
+}
+
+export default App;
